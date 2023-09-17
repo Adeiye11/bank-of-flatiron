acc7cda80f4849b5ef4325b3de6537828184c7d0
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-17 23:16:12 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TransactionsTable from './Components/TransactionsTable';
+import { useEffect,useState } from 'react';
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [category, setCategory] = useState('all');
+  const [search, setSearch] = useState('');
+  const [date, setDate] = useState('');
+
+  function onCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function onSearchChange(event) {
+    setSearch(event.target.value);
+  }
+
+  function onDateChange(event) {
+    setDate(event.target.value);
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3000/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>My Transactions</h1>
+      <div className="filters">
+        <div className="filter">
+        <label htmlFor="filter">Filter by category:</label>
+        <select name="filter" id="filter" onChange={(e) => onCategoryChange(e)}>
+          <option value="all">All</option>
+          <option value="Income">Income</option>
+          <option value="Food">Food</option>
+          <option value="Transportation">Transportation</option>
+          <option value="Housing">Housing</option>
+          <option value="Fashion">Fashion</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Gift">Gift</option>
+        </select>
+        </div>
+        <div className="filter">
+        <label htmlFor="date">Date:</label>
+        <input type="date" id="date" onChange={(e)=>onDateChange(e)} />
+        </div>
+      <div className="filter">
+        <label htmlFor="search">Search:</label>
+        <input type="text" id="search" onChange={(e)=>onSearchChange(e)} />
+        </div>
+      </div>
+      </div>
+      <div className="main">
+          <TransactionsTable transactions={transactions} category={category} search={search} date={date} />
+          <div className='new-transaction'>
+            <h2>New Transaction</h2>
+            <form action='http://localhost:3000/transactions' method='POST'>
+              <label htmlFor="category">Category:</label>
+              <select name="category" id="category">
+                <option value="Income">Income</option>
+                <option value="Food">Food</option>
+                <option value="Transportation">Transportation</option>
+                <option value="Housing">Housing</option>
+                <option value="Fashion">Fashion</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Gift">Gift</option>
+              </select>
+              <label htmlFor="description">Description:</label>
+              <input type="text" id="description" name='description' />
+              <label htmlFor="amount">Amount:</label>
+              <input type="number" id="amount" name='amount' />
+              <label htmlFor="date">Date:</label>
+              <input type="date" id="date" name='date' />
+              <input type="submit" value="Add Transaction" />
+            </form>
+            </div>
+        </div>
     </div>
   );
 }
